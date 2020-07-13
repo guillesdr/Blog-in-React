f3212eb56d952b8acb4a93866a90ddcfbb62654c
carobsts <bsts.carolina@gmail.com>
2020-07-13 01:33:25 -0300

    Card component was created.

---
+++ src/pages/Home/components/card/Card.jsx
@@ -0,0 +1,28 @@
+import React, { memo } from 'react';
+import './Card.css';
+import { Link } from 'react-router-dom';
+
+const Card = ({ post }) => {
+
+    const { slug, coverImage, coverImageAlt, title, datePretty, content } = post;
+
+    return (
+        <section className="card">
+            <img src={coverImage} alt={coverImageAlt} />
+            <div className="card__content">
+                <h2>
+                    {title} &mdash;{" "}
+                <span style={{ color: "#5e5e5e" }}>{datePretty}</span>
+                </h2>
+                <p
+                dangerouslySetInnerHTML={{
+                    __html: `${content.substring(0, 200)}...`
+                }}
+                ></p>
+                <Link to={`/post/${slug}`} className='card__content--link'> + Leer </Link>
+            </div>
+        </section>
+    )
+};
+
+export default Card;
